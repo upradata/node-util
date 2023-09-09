ab6064bc46163779743d4213b8b633a1c823b386
El Pupi <a01633814@unet.univie.ac.at>
2023-09-09 13:08:14 +0200

    computer reinstallation files metadata changed

--- src/cache/common.ts
+++
@@ -1,4 +0,0 @@
-import { yellow } from '../template-style';
-
-
-export const warn = (msg: string) => console.warn(yellow`>> ${msg}`);
--- test/cache/store.spec.ts
+++
@@ -1,6 +0,0 @@
-
-
-describe('Test suite for store', () => {
-    it('test', () => { });
-
-});
